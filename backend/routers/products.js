53ac064d83180a13fd531bfa9dfaaa1ce0eb099f
yunjae <juj7347@naver.com>
2021-11-04 16:11:54 +0900

    image upload

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -5,13 +5,26 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/uploads');
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if(isValid) {
+            uploadError = null;
+        }
+        cb(uploadError, 'public/uploads');
     },
-    filename: function (req, file, cb) {
+    filename: function (req, file, cb) { 
         const fileName = file.originalname.split(' ').join('-');
-        cb(null, fileName + '-' + Date.now());
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
     }
 })
 
@@ -56,14 +69,20 @@ router.post(`/`, uploadOptions.single('image'), async (req, res)=>{
     if(!category) {
         return res.status(400).send('Invalid Category');
     }
-    const fileName =  req.fileName;
+
+    const file = req.file;
+    if(!file) {
+        return res.status(400).send('No image in request');
+    }
+
+    const fileName =  file.filename;
     const basePath = `${req.protocol}://${req.get('host')}/public/upload/`;
 
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: fileName,
+        image: `${basePath}${fileName}`,
         price: req.body.price,
         category: req.body.category,
         dataCreated: req.body.dataCreated
