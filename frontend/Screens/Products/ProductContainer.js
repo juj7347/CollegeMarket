f07f92f153f9cc8b66e4a1bd6f36e82798dceae9
yunjae <juj7347@naver.com>
2021-11-05 14:19:36 +0900

    searched products created, begore native base installed

--- frontend/Screens/Products/ProductContainer.js
+++ frontend/Screens/Products/ProductContainer.js
@@ -8,9 +8,11 @@ const data = require('../../assets/data/products.json');
 const ProductContainer = () => {
 
     const [products, setProducts] = useState([]);
+    const [productsFiltered, setProductsFiltered] = useState([]);
 
     useEffect(() => {
         setProducts(data);
+        setProductsFiltered(data);
 
         return () => {
             setProducts([]);
---
+++ frontend/Screens/Products/SearchedProducts.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const SearchedProducts = () => {
+
+}
+
+export default SearchedProducts;
