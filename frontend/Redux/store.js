fb228fe4f912b364bd4ca005e0f8d80346758bcf
yunjae <juj7347@naver.com>
2021-11-07 15:13:04 +0900

    redux init

---
+++ frontend/Redux/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
+
+const reducers = combineReducers({
+    
+})
