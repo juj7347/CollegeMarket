ab65b84522e0b8e228b4147c5119164d63947a86
tkdgus96 <tkdgus96@gmail.com>
2021-11-11 20:36:47 +0900

    AppBar Update

--- frontend/App.js
+++ frontend/App.js
@@ -12,15 +12,20 @@ import {
   VStack,
   Code,
 } from "native-base";
-import { NavigationContainer } from "@react-navigation/native";
+import { 
+  NavigationContainer,
+  useNavigation 
+} from "@react-navigation/native";
+import { RectButton, BorderlessButton } from "react-native-gesture-handler";
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { BottomTabBar, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import LoginPage from "./components/Body/Login/LoginPage";
 import AppBar from "./components/Header/AppBar";
 import BottomBar from "./components/Footer/BottomBar";
 import Home from "./components/Body/Home";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -28,20 +33,61 @@ const Drawer = createDrawerNavigator();
 function MyDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="MyPage" component={LoginPage}/>
+      <Drawer.Screen name="Home" component={Home} 
+      options={{
+        headerRight: (props) => {
+          const navigation = useNavigation();
+          return (
+            <BorderlessButton
+              onPress={() => navigation.navigate("SearchScreen")}
+              style={{ marginRight: 15 }}
+            >
+              <Ionicons
+                name="md-search"
+                size={Platform.OS === "ios" ? 22 : 25}
+              />
+            </BorderlessButton>
+          )
+         }
+         }}/>
+      <Drawer.Screen name="Chat" component={LoginPage}
+      options={{
+        headerRight: (props) => {
+          const navigation = useNavigation();
+          return (
+            <BorderlessButton
+              onPress={() => navigation.navigate("SearchScreen")}
+              style={{ marginRight: 15 }}
+            >
+              <Ionicons
+                name="md-search"
+                size={Platform.OS === "ios" ? 22 : 25}
+              />
+            </BorderlessButton>
+          )
+         }
+         }}/>
     </Drawer.Navigator>
   )
 }
-
-
+function SearchScreen() {
+  return (
+    <Text> Search Screen </Text>
+  )
+}
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-      <MyDrawer/>
       <Tab.Navigator
         screenOptions={({ route }) => ({
+          tabBarButton: [
+            "SearchScreen",
+          ].includes(route.name)
+            ? () => {
+                return null;
+              }
+            : undefined,
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
@@ -66,10 +112,65 @@ export default function App() {
           tabBarInactiveTintColor: 'gray',
         })}
       >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="MyPage" component={LoginPage}/>
-          <Tab.Screen name="Product" component={Home}/>
-          <Tab.Screen name="Chat" component={LoginPage}/>
+          <Tab.Screen name="Home" component={Home}
+          options={{
+            headerRight: (props) => {
+              const navigation = useNavigation();
+              return (
+                <BorderlessButton
+                  onPress={() => navigation.navigate("SearchScreen")}
+                  style={{ marginRight: 15 }}
+                >
+                  <Ionicons
+                    name="md-search"
+                    size={Platform.OS === "ios" ? 22 : 25}
+                  />
+                </BorderlessButton>
+              )
+             }
+             }} />
+          <Tab.Screen name="MyPage" component={LoginPage}
+          options={{
+            headerRight: (props) => {
+              const navigation = useNavigation();
+              return (
+                <BorderlessButton
+                  onPress={() => navigation.navigate("SearchScreen")}
+                  style={{ marginRight: 15 }}
+                >
+                  <Ionicons
+                    name="md-search"
+                    size={Platform.OS === "ios" ? 22 : 25}
+                  />
+                </BorderlessButton>
+              )
+             }
+             }}
+          />
+          <Tab.Screen name="Product" component={Home}
+          options={{
+            headerRight: (props) => {
+              const navigation = useNavigation();
+              return (
+                <BorderlessButton
+                  onPress={() => navigation.navigate("SearchScreen")}
+                  style={{ marginRight: 15 }}
+                >
+                  <Ionicons
+                    name="md-search"
+                    size={Platform.OS === "ios" ? 22 : 25}
+                  />
+                </BorderlessButton>
+              )
+             }
+             }}/>
+          <Tab.Screen 
+            name="Chat" 
+            component={MyDrawer}
+            options={{ headerShown: false }}/>
+          <Tab.Screen
+            name="SearchScreen"
+            component={SearchScreen}/>
         </Tab.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
