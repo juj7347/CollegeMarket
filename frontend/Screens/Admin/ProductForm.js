b3d0b50f615e8fdec479b225fe51e043672d559a
yunjae <juj7347@naver.com>
2021-11-19 19:27:12 +0900

    product post form

--- frontend/Screens/Admin/ProductForm.js
+++ frontend/Screens/Admin/ProductForm.js
@@ -22,11 +22,63 @@ import baseURL from "../../assets/common/baseURL";
 import axios from "axios";
 
 const ProductForm = (props) => {
+
+    const [pickerValue, setPickerValue] = useState();
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState();
+    const [mainImage, setMainImage] = useState('');
+    const [category, setCategory] = useState();
+    const [categories, setCategories] = useState([]);
+    const [token, setToken] = useState();
+    const [error, setError] = useState();
+    const [isFeatured, setIsFeatured] = useState();
+    const [rickDescription, setRichDescription] = useState();
+    const [item, setItem] = useState();
+
+
     return (
-        <View>
-            <Text>ProductForm</Text>
-        </View>
+        <FormContainer
+            title="Add Product"
+        >
+            <View>
+                <Image source={{uri: mainImage}}/>
+                <TouchableOpacity>
+                    <Text>Image</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Input
+                placeholder="글 제목"
+                name="name"
+                id="name"
+                value={name}
+                onChangeText={(text)=>setName(text)}
+            />
+            <Input
+                placeholder="가격"
+                name="price"
+                id="price"
+                value={price}
+                onChangeText={(text)=>setPrice(text)}
+            />
+            <Input
+                placeholder="게시글 내용을 작성해주세요"
+                name="description"
+                id="description"
+                value={description}
+                onChangeText={(text)=>setDescription(text)}
+            />
+        </FormContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    label: {
+        width: '80%',
+        marginTop: 10
+    }
+})
 
 export default ProductForm;
